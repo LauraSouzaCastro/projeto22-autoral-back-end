14fcad603f9aaa49bda99c82ca57441d6d930d3a
Laura <laurasouzacastro1@gmail.com>
2023-06-26 10:33:47 -0300

    fix: imports

---
+++ tests/factories/transactions-factory.ts
@@ -0,0 +1,35 @@
+import { faker } from '@faker-js/faker';
+import { prisma } from '../../src/config';
+import { TransactionType } from '.prisma/client';
+
+export async function createTransaction(userId: number, typeTransaction: TransactionType) {
+    const category = await prisma.category.create({
+        data: {
+            userId,
+            color: faker.color.rgb(),
+            name: faker.string.alphanumeric(),
+        }
+    })
+
+    const transaction = await prisma.transaction.create({
+        data: {
+            typeTransaction,
+            value: 1,
+            dateTransaction: faker.date.recent(),
+            done: true,
+            categoryId: category.id,
+            userId,
+        },
+    });
+
+    await prisma.balance.create({
+        data: {
+            transactionId: transaction.id,
+            dateTransaction: transaction.dateTransaction,
+            value: transaction.value,
+            userId: transaction.userId,
+        }
+    });
+
+    return transaction;
+}
